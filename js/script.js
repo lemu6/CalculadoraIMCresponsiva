e0fb095a99de9e121c6ba3be58fb3e3ce5345542
LemuDev <lemu200011@gmail.com>
2024-03-03 22:50:06 -0300

    js 2.0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 const form = document.getElementById('form');
+
+form.getEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const weigth = document.getElementById('weight').value;
+    const height = document.getElementById('heigth').value;
+
+    const bmi = (weigth / (height * height)).toFixed(2);
+
+    const value = document.getElementById('value')
+});
